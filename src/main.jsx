6c7b8d91373a574103ecadca0caddb22b1e7df2f
ISMAIL ALY YOUSSEF ELNAGGAR <ismail@192.168.1.4>
2026-01-30 21:07:45 +0200

    Fix: Remove missing animations.css import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import './index.css';
-import './animations.css';
 
 console.log('🚀 Starting application...');
 
